257e786a64f4fa3fa235ca65edf11186605672a5
DESKTOP-82JLC2M\Air <vvartanyan@mentalstack.com>
2019-05-15 21:12:34 +0300

    MapKit experiments create routes by click

--- app/src/main/java/com/air/yandexmapkit/MainActivity.kt
+++ app/src/main/java/com/air/yandexmapkit/MainActivity.kt
@@ -4,19 +4,36 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.yandex.mapkit.Animation
 import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.RequestPoint
+import com.yandex.mapkit.RequestPointType
 import com.yandex.mapkit.directions.DirectionsFactory
-import com.yandex.mapkit.directions.driving.AnnotationLanguage
-import com.yandex.mapkit.directions.driving.VehicleType
+import com.yandex.mapkit.directions.driving.DrivingOptions
+import com.yandex.mapkit.directions.driving.DrivingRoute
+import com.yandex.mapkit.directions.driving.DrivingSession
 import com.yandex.mapkit.geometry.Point
 import com.yandex.mapkit.map.CameraPosition
+import com.yandex.mapkit.map.MapObjectCollection
 import com.yandex.mapkit.user_location.UserLocationTapListener
 import kotlinx.android.synthetic.main.activity_main.*
+import com.yandex.mapkit.directions.driving.DrivingSession.DrivingRouteListener as DrivingRouteListener1
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),DrivingSession.DrivingRouteListener {
+    private var mapObjects: MapObjectCollection? = null
+    override fun onDrivingRoutesError(p0: com.yandex.runtime.Error) {
+        print("")
+    }
+
+    override fun onDrivingRoutes(p0: MutableList<DrivingRoute>) {
+        val d = p0[0]
+        p0.forEach {
+            mapObjects?.addPolyline(it.geometry)
+        }
+        print("")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        MapKitFactory.setApiKey("cce58919-fc3a-401a-ae13-aaa1d03284da")
+        MapKitFactory.setApiKey("Key")
         MapKitFactory.initialize(this)
         setContentView(R.layout.activity_main)
         super.onCreate(savedInstanceState)
@@ -35,7 +52,16 @@ class MainActivity : AppCompatActivity() {
 
         btn.setOnClickListener {
             mapview.map.isDebugInfoEnabled = true
-            // textview.text = mapview.map.
+            DirectionsFactory.initialize(this)
+            val drvRouter = DirectionsFactory.getInstance().createDrivingRouter()
+            val drOption = DrivingOptions()
+            drOption.alternativeCount = 3
+            val repoint1 = RequestPoint(Point(55.751574, 37.573856), RequestPointType.WAYPOINT, "")
+            val repoint2 = RequestPoint(Point(55.751574, 38.573856), RequestPointType.WAYPOINT, "")
+            val arr = arrayListOf<RequestPoint>(repoint1, repoint2)
+            val drvSession = drvRouter.requestRoutes(arr, drOption, this)
+            drvSession.retry(this)
+            drvRouter.resume()
         }
         mapview.map.userLocationLayer.isEnabled = true
 
@@ -43,14 +69,8 @@ class MainActivity : AppCompatActivity() {
             textview.text = "${it.latitude} ${it.longitude}"
         }
         mapview.map.userLocationLayer.setTapListener(ssd)
+        mapObjects = mapview.map.mapObjects.addCollection()
 
-        DirectionsFactory.initialize(this)
-        val s = DirectionsFactory.getInstance().createDrivingRouter()
-        s.setVehicleType(VehicleType.DEFAULT)
-        s.setAnnotationLanguage(AnnotationLanguage.RUSSIAN)
-        s.resume()
-        s.
-        val f = s.routeSerializer()
     }
 
     override fun onStart() {
