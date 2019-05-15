f7a139fcb4b058ae76377c6cc7eb02404986d3b5
Vladimir_Vartanyan <vvartanyan@mentalstack.com>
2019-05-15 20:34:59 +0300

    map  has been initialized

--- app/src/main/java/com/air/yandexmapkit/MainActivity.kt
+++ app/src/main/java/com/air/yandexmapkit/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.air.yandexmapkit
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.yandex.mapkit.Animation
+import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.map.CameraPosition
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+
+        MapKitFactory.setApiKey("cce58919-fc3a-401a-ae13-aaa1d03284da")
+        MapKitFactory.initialize(this)
         setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState)
+        mapview.map.move(
+            CameraPosition(Point(55.751574, 37.573856), 11.0f, 0.0f, 0.0f),
+            Animation(Animation.Type.SMOOTH, 0f), null)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapview.onStart()
+        MapKitFactory.getInstance().onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapview.onStop()
+        MapKitFactory.getInstance().onStop()
     }
 }
